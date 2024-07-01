90cadc22ae37531c8977226ba97da97c4d233c13
Nolann Bougrainville <nolann.bougrainville@epitech.eu>
2024-07-01 10:47:22 +0200

    Initial Commit

---
+++ onvayarriver/onvayarriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  onvayarriverApp.swift
+//  onvayarriver
+//
+//  Created by Nolann Bougrainville on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct onvayarriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
